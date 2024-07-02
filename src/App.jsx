edd32a59bdc86a796d33fe27bb8b3ec219c6a6e3
Naveenaa28 <naveenaaramesh@gmail.com>
2024-07-02 20:27:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; 
+import StopWatch from './stopwatch';
+
+const App = () => {
+  return (
+    <div>
+      <StopWatch />
+    </div>
+  );
+};
+
+export default App;
